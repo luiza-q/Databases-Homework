79b3d72b62f32296e51ec85a377c38dfb7ca13d5
Luiza Quaglio <luizaq@gmail.com>
2021-11-13 21:11:54 +0100

    homework week 3 complete on week 2 folder

--- week-2/mandatory/api/api.js
+++ week-2/mandatory/api/api.js
@@ -20,16 +20,28 @@ const api = () => {
         });
     };
 
-    const getProductsAndSuppliers = (req, res) => {
-        const name = req.query.name;
-        pool.query(
-            "SELECT p.product_name as product, s.supplier_name as supplier FROM products as p INNER JOIN suppliers as s ON s.id=p.supplier_id", (error, result) => {
-          res.json(result.rows);
-        });
-    }
+    const getProducts = async (req, res) => {
+      try{
+        const searchProduct = req.query.searchProduct;
+        if(searchProduct) {
+          productQuery = await pool.query (`select * from products where product_name like '%${searchProduct}%'`
+          );
+          if (productQuery.rows.length > 0) {
+            return res.send(productQuery.rows);
+          } else {
+            return res.status(404).send('product not found')
+          }
+        } else {
+          const result = await pool.query(`select * from products`);
+          return res.status(200).send(result.rows)        }
+
+      } catch(err){
+        console.log(err);
+      }
+    };
 
     const getCustomerById = (req, res) => {
-        const customerId = req.params.id;
+        const customerId = req.params.customerId;
         pool
         .query(`SELECT * FROM customers where id=$1;`, [customerId])
           .then((result) => res.json(result.rows))
@@ -65,10 +77,10 @@ const api = () => {
         const newProductPrice = req.body.unit_price;
         const newProductSupplierId = req.body.supplier_id;
        
-        if (!Number.isInteger(newProductPrice) || newProductPrice <= 0) {
+        if (!Number.isInteger(newProductPrice) || newProductPrice <= 0 || newProductSupplierId.rows.length === 0) {
            return res
              .status(400)
-             .send("Price should be a positive number");
+             .send("Product is not valid: price must be positive and supplier must exist");
         } else {
             const query = "INSERT INTO products (product_name, unit_price, supplier_id) VALUES ($1, $2, $3)";
             pool
@@ -77,20 +89,113 @@ const api = () => {
            .catch((e) => console.error(e));
         }
     }
-        
+
+    const insertNewOrder = async (req, res) => {
+      const customerId = req.params.customerId;
+      const newOrderDate = req.body.order_date;
+      const newOrderRef = req.body.order_reference;
+      
+      const checkCustomerId = await pool.query("SELECT * FROM orders WHERE customer_id=$1", [customerId])
+      if (checkCustomerId > 0) {
+        return res.status(400).send("Please choose an existing customer");
+        } 
+          
+      const query = "INSERT INTO orders (order_date, order_reference, customer_id) VALUES ($1, $2, $3)";
+      const result = await pool.query(query, [newOrderDate, newOrderRef, customerId])
+              
+      return res.status(200).send("Order created!").json(result.rows);
+            
+    }
+    
+    const updateCustomer = async (req, res) => {
+      try {
+        const customerId = req.params.customerId;
+        const customer = req.body;
+        const query = "UPDATE customers SET name=$1, address=$2, city=$3, country=$4 WHERE id=$5;";
+        const result = await pool.query(query, [
+          customer.name, 
+          customer.address, 
+          customer.city, 
+          customer.country, 
+          customerId]);  
+        return res.status(200).send("Customer updated").json(result.rows);
+      }
+      catch (e){
+          return res.status(500).send("Error");
+      }
+    }
+
+    const deleteOrder = async (req, res) => {
+      try {
+        const orderId = req.params.orderId;
+        const queryOrdersItems = "delete from order_items where order_id=$1";
+        const queryOrders = "delete from orders where id=$1;";
+        await pool.query(queryOrdersItems, [orderId]);
+        const result = await pool.query(queryOrders, [orderId]);
+        return res.status(200).send("order deleted").json(result.rows);
+      }
+      catch (e){
+          return res.status(500).send("Error");
+      }
+
+    }
+
+    const deleteCustomer = async (req, res) => {
+      try{
+        const customerId = req.params.customerId;
+        const queryOrders = "select * from orders where customer_id=$1"
+        const checkOrders = await pool.query(queryOrders, [customerId]);
+        if (checkOrders.rows.length > 0) {
+          return res.status(400).send("Cannot delete customer: customer has active orders");
+        } else {
+          const queryCustomer = "delete from customers where id=$1"
+          const result = await pool.query(queryCustomer, [customerId]);
+          return res.status(200).send("customer deleted").json(result.rows);
+        }
+
+      }
+      catch (e){
+        return res.status(500).send(e);
+      }
+
+    }
+
+    const getOrderDetails = async (req, res) => {
+      try{
+      const customerId = req.params.customerId;
+      const query = `select c.name as customerName, o.order_reference, o.order_date, 
+      p.product_name, p.unit_price, s.supplier_name, i.quantity 
+      from orders o inner join customers c on o.customer_id=c.id
+      inner join order_items i on i.order_id=o.id
+      inner join products p on i.product_id=p.id
+      inner join suppliers s on s.id=p.supplier_id
+      where c.id=$1`;
+      const result = await pool.query(query, [customerId]);
+      return res.status(200).json(result.rows);
+      }
+      catch (e) {
+        return res.status(500).send(e);
+      }
+
+    }
+
+
 
 
 
 
     return {
-        getAllCustomers,
-        getAllSuppliers,
-        getProductsAndSuppliers,
-        getCustomerById,
-        insertNewCustomer,
-        insertNewProduct,
-        
-
+      getAllCustomers,
+      getAllSuppliers,
+      getProducts,
+      getCustomerById,
+      insertNewCustomer,
+      insertNewProduct,
+      insertNewOrder,
+      updateCustomer,
+      deleteOrder,
+      deleteCustomer,
+      getOrderDetails
     };
 };
 
--- week-2/mandatory/api/server.js
+++ week-2/mandatory/api/server.js
@@ -3,15 +3,27 @@ const app = express();
 const port = 4000;
 const apiFunction = require("./api.js");
 const api = apiFunction();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-
+//get endpoints
 app.get("/customers", api.getAllCustomers);
 app.get("/suppliers", api.getAllSuppliers);
-app.get("/products", api.getProductsAndSuppliers);
-app.get("/customers/:customerId", api.getCustomerById); 
+app.get("/products", api.getProducts);
+app.get("/customers/:customerId", api.getCustomerById);
+app.get("/customers/:customerId/orders", api.getOrderDetails);
 
+//post endpoints
 app.post("/customers", api.insertNewCustomer);
 app.post("/products", api.insertNewProduct);
+app.post("/customers/:customerId/orders", api.insertNewOrder);
+
+//put endpoints
+app.put("/customers/:customerId", api.updateCustomer);
+
+//delete endpoints
+app.delete("/orders/:orderId", api.deleteOrder);
+app.delete("/customers/:customerId", api.deleteCustomer);
 
 
 app.listen(port, () => console.log(`app listening to port: ${port}`));
